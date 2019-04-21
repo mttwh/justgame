beacbfd66baa04569e381cb660fda8a667f43106
mttwh <mttw5682@gmail.com>
2019-04-20 20:36:11 -0400

    Added basic user login and registration

---
+++ routes/users.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const bcrypt = require('bcryptjs');
+
+var expressValidator = require('express-validator');
+router.use(expressValidator());
+
+let User = require('../models/user');
+
+
+router.get('/register', function(req, res){
+    res.sendFile(path.join(__dirname + '/../public/register.html'));
+});
+
+//Register User
+router.post('/register', function(req, res){
+    const name = req.body.name;
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+    const passwordConfirmation = req.body.passwordConfirmation;
+
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('passwordConfirmation', 'Passwords do not match').equals(req.body.password);
+
+    let errors = req.validationErrors();
+
+    if(errors){
+        res.sendFile(path.join(__dirname + '/../public/register.html'));
+    } else {
+        let newUser = new User({
+            name:name,
+            email:email,
+            username:username,
+            password:password
+        });
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(newUser.password, salt, function(err, hash){
+                if(err){
+                    console.log(err);
+                }
+                newUser.password = hash;
+                newUser.save(function(err){
+                    if(err){
+                        console.log(err);
+                        return;
+                    } else {
+                        //req.flash('success', 'You are now registered and can log in');
+                        res.redirect('/users/login');
+                    }
+                });
+            });
+        });
+    }
+});
+
+
+//Login route
+router.get('/login', function(req, res){
+    res.sendFile(path.join(__dirname + '/../public/login.html'));
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,56 @@
-var express = require('express');
-var fs = require('fs')
-var app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+
+//Initialize app
+const app = express();
+
+//check connection
+db.once('open', function(){
+  console.log('Connected to MongoDB');
+});
+
+
+//check for db errors
+db.on('error', function(err){
+  console.log(err);
+});
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+
 let port = process.env.PORT;
+
+let User = require('./models/user');
 
 app.use(express.static('public'));
 
 if (port == null || port == "") {
   port = 8000;
 }
+
+//Home route
+app.get('/', function(req, res){
+  User.find({}, function(err, users){
+    if(err){
+      console.log(err);
+    } else {
+        res.sendFile(path.join(__dirname + '/public/index.html'));
+    }
+  });
+})
+
+//route files
+let users = require('./routes/users');
+app.use('/users', users);
 
 app.listen(port, () => console.log('Node server listening on port ' + port));
 
